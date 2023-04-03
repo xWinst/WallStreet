b976860aae7ecc69362ffe877cdd1ea2a884c58b
Oleg <hellreebok@gmail.com>
2023-04-03 15:30:36 +0300

    update HomeMenu

--- src/components/HomeMenu/HomeMenu.jsx
+++ src/components/HomeMenu/HomeMenu.jsx
@@ -5,12 +5,12 @@ import s from './HomeMenu.module.css';
 const HomeMenu = () => {
     const navigate = useNavigate();
 
-    const play = () => {};
+    // const play = () => {};
 
     return (
         <div className={s.container}>
             <p className={s.title}>Wall Street</p>
-            <Button cn={s.btn} text="Играть" click={() => play} />
+            {/* <Button cn={s.btn} text="Играть" click={() => play} /> */}
             <Button cn={s.btn} text="Войти" click={() => navigate('/login')} />
             <Button
                 cn={s.btn}
